1052429e0356bad78791032a439ed50b70133297
NoReply Amazon <no-reply@amazon.com>
2024-12-10 06:19:47 +0000

    Revert "[Commit] Iteration 47 #errors = 96 <= 103."
    This reverts commit e85307e440196f66aae25cc348d5d838e0f4c4c1.

--- app/Bookstore.Domain/Repositories/ReferenceDataRepository.cs
+++ app/Bookstore.Domain/Repositories/ReferenceDataRepository.cs
@@ -9,28 +9,6 @@ using System.Threading.Tasks;
 
 namespace Bookstore.Data.Repositories
 {
-    public class PaginatedList<T> : List<T>, IPaginatedList<T>
-    {
-        public int PageIndex { get; private set; }
-        public int TotalPages { get; private set; }
-
-        public PaginatedList(IQueryable<T> source, int pageIndex, int pageSize)
-        {
-            PageIndex = pageIndex;
-            TotalPages = (int)Math.Ceiling(source.Count() / (double)pageSize);
-            this.AddRange(source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList());
-        }
-
-        public bool HasPreviousPage => PageIndex > 1;
-
-        public bool HasNextPage => PageIndex < TotalPages;
-
-        public Task PopulateAsync()
-        {
-            // Implement if needed
-            return Task.CompletedTask;
-        }
-    }
 
     public class ReferenceDataRepository : IReferenceDataRepository
     {
@@ -56,19 +34,21 @@ namespace Bookstore.Data.Repositories
             return await dbContext.ReferenceData.ToListAsync();
         }
 
-    public Task<IPaginatedList<ReferenceDataItem>> ListAsync(ReferenceDataFilters filters, int pageIndex, int pageSize)
-    {
-        var query = dbContext.ReferenceData.AsQueryable();
-
-        if (filters.ReferenceDataType.HasValue)
+        public async Task<IPaginatedList<ReferenceDataItem>> ListAsync(ReferenceDataFilters filters, int pageIndex, int pageSize)
         {
-            query = query.Where(x => x.DataType == filters.ReferenceDataType.Value);
+            var query = dbContext.ReferenceData.AsQueryable();
+
+            if (filters.ReferenceDataType.HasValue)
+            {
+                query = query.Where(x => x.DataType == filters.ReferenceDataType.Value);
+            }
+
+            var result = new PaginatedList<ReferenceDataItem>(query, pageIndex, pageSize);
+
+            await result.PopulateAsync();
+
+            return result;
         }
-
-        var result = new PaginatedList<ReferenceDataItem>(query, pageIndex, pageSize);
-
-        return Task.FromResult<IPaginatedList<ReferenceDataItem>>(result);
-    }
 
         public async Task SaveChangesAsync()
         {
