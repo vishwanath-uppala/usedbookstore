9c1c4c5ba029c6804bde50220679c2c4b5051476
NoReply Amazon <no-reply@amazon.com>
2024-12-10 06:07:51 +0000

    [Commit] Iteration 30 #errors = 106 <= 106.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Domain/Repositories/ReferenceDataRepository.cs][CS0539][37,80] 'ReferenceDataRepository.ListAsync(ReferenceDataFilters, int, int)' in explicit interface declaration is not found among members of the interface that can be implemented``.

--- app/Bookstore.Domain/Repositories/ReferenceDataRepository.cs
+++ app/Bookstore.Domain/Repositories/ReferenceDataRepository.cs
@@ -34,7 +34,7 @@ namespace Bookstore.Data.Repositories
             return await dbContext.ReferenceData.ToListAsync();
         }
 
-        async Task<IPaginatedList<ReferenceDataItem>> IReferenceDataRepository.ListAsync(ReferenceDataFilters filters, int pageIndex, int pageSize)
+        IPaginatedList<ReferenceDataItem> IReferenceDataRepository.ListAsync(ReferenceDataFilters filters, int pageIndex, int pageSize)
         {
             var query = dbContext.ReferenceData.AsQueryable();
 
@@ -45,7 +45,7 @@ namespace Bookstore.Data.Repositories
 
             var result = new PaginatedList<ReferenceDataItem>(query, pageIndex, pageSize);
 
-            await result.PopulateAsync();
+            result.Populate();
 
             return result;
         }
