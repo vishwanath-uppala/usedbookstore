10d735fc11a7e53ff7235447dd691f50ec03875f
NoReply Amazon <no-reply@amazon.com>
2024-12-10 06:44:55 +0000

    Revert "[Commit] Iteration 80 #errors = 14 <= 13."
    This reverts commit 55f2dde1b0656aed2330f367c47f80a1fd43530e.

--- app/Bookstore.Web/Controllers/AuthenticationController.cs
+++ app/Bookstore.Web/Controllers/AuthenticationController.cs
@@ -8,13 +8,6 @@ namespace Bookstore.Web.Controllers
 {
     public class AuthenticationController : Controller
     {
-        private readonly IBookstoreConfiguration _bookstoreConfiguration;
-
-        public AuthenticationController(IBookstoreConfiguration bookstoreConfiguration)
-        {
-            _bookstoreConfiguration = bookstoreConfiguration;
-        }
-
         public ActionResult Login(string redirectUri = null)
         {
             if(string.IsNullOrWhiteSpace(redirectUri)) return RedirectToAction("Index", "Home");
@@ -24,7 +17,7 @@ namespace Bookstore.Web.Controllers
 
         public ActionResult LogOut()
         {
-            return _bookstoreConfiguration.Get("Services/Authentication") == "aws" ? CognitoSignOut() : LocalSignOut();
+            return BookstoreConfiguration.Get("Services/Authentication") == "aws" ? CognitoSignOut() : LocalSignOut();
         }
 
         private ActionResult LocalSignOut()
