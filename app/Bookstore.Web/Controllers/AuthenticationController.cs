96ed2ec9a2db9c2018798c73af09e94645b7a403
NoReply Amazon <no-reply@amazon.com>
2024-12-10 06:52:57 +0000

    [Commit] Iteration 90 #errors = 4 <= 5.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Controllers/AuthenticationController.cs][CS0433][39,26] The type 'BookstoreConfiguration' exists in both 'Bookstore.Domain, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null' and 'Bookstore.Data, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'``.

--- app/Bookstore.Web/Controllers/AuthenticationController.cs
+++ app/Bookstore.Web/Controllers/AuthenticationController.cs
@@ -2,11 +2,20 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using BobsBookstoreClassic.Data;
+using Bookstore.Domain;
+using Bookstore.Data;
 
 namespace Bookstore.Web.Controllers
 {
     public class AuthenticationController : Controller
     {
+        private readonly IBookstoreConfiguration _configuration;
+
+        public AuthenticationController(IBookstoreConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public ActionResult Login(string redirectUri = null)
         {
             if(string.IsNullOrWhiteSpace(redirectUri)) return RedirectToAction("Index", "Home");
@@ -16,7 +25,7 @@ namespace Bookstore.Web.Controllers
 
         public ActionResult LogOut()
         {
-            return BookstoreConfiguration.Get("Services/Authentication") == "aws" ? CognitoSignOut() : LocalSignOut();
+            return _configuration.Get("Services/Authentication") == "aws" ? CognitoSignOut() : LocalSignOut();
         }
 
         private ActionResult LocalSignOut()
@@ -36,8 +45,8 @@ namespace Bookstore.Web.Controllers
                 Response.Cookies.Delete(".AspNet.Cookies");
             }
 
-            var domain = BookstoreConfiguration.Get("Authentication/Cognito/CognitoDomain");
-            var clientId = BookstoreConfiguration.Get("Authentication/Cognito/LocalClientId");
+            var domain = _configuration.Get("Authentication/Cognito/CognitoDomain");
+            var clientId = _configuration.Get("Authentication/Cognito/LocalClientId");
             var logoutUri = $"{Request.Scheme}://{Request.Host}/";
 
             return Redirect($"{domain}/logout?client_id={clientId}&logout_uri={logoutUri}");
