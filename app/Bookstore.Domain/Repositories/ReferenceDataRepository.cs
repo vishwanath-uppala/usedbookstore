d0158030c5ce2a4ed1483edd96cec7c9d3f4e098
NoReply Amazon <no-reply@amazon.com>
2024-12-10 06:17:42 +0000

    Revert "[Commit] Iteration 44 #errors = 109 <= 103."
    This reverts commit 47735a790896c033056c81e7e9b2f138fa86953b.

--- app/Bookstore.Domain/Repositories/ReferenceDataRepository.cs
+++ app/Bookstore.Domain/Repositories/ReferenceDataRepository.cs
@@ -9,22 +9,6 @@ using System.Threading.Tasks;
 
 namespace Bookstore.Data.Repositories
 {
-    public class PaginatedListWrapper<T> : IPaginatedList<T>
-    {
-        private readonly PaginatedList<T> _innerList;
-
-        public PaginatedListWrapper(PaginatedList<T> innerList)
-        {
-            _innerList = innerList;
-        }
-
-        public int PageIndex => _innerList.PageIndex;
-        public int TotalPages => _innerList.TotalPages;
-        public int TotalCount => _innerList.TotalCount;
-        public bool HasPreviousPage => _innerList.HasPreviousPage;
-        public bool HasNextPage => _innerList.HasNextPage;
-        public List<T> Items => _innerList.Items;
-    }
 
     public class ReferenceDataRepository : IReferenceDataRepository
     {
@@ -63,7 +47,7 @@ namespace Bookstore.Data.Repositories
 
             await result.PopulateAsync();
 
-            return new PaginatedListWrapper<ReferenceDataItem>(result);
+            return result;
         }
 
         public async Task SaveChangesAsync()
